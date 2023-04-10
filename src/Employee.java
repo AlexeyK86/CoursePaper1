7ecc99e3f0a62470809f1da1bc91ef442eab3982
Алексей <alesha1986@bk.ru>
2023-04-10 08:23:34 +0300

    CoursePaper1

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,61 @@
 public class Employee {
+    public final int id;
+    public String fullName;
+    public int department;
+    public int salary;
+
+    public static int counter = 1;
+
+    public Employee(String fullName, int department, int salary) {
+        this.id = counter++;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void setCounter(int counter) {
+        Employee.counter = counter;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
 public class Main {
+
+    public static Employee[] employees = {
+            new Employee("Nikita Kuznetsov Петрович", 1, 80000),
+            new Employee("Sergey Ivanov Семенович", 2, 60000),
+            new Employee("Marina Semenova Валерьевна", 3, 115000),
+            new Employee("Anna Nikolaeva Ивановна", 4, 180000),
+            new Employee("Elena Solovyeva Дмитриевна", 5, 120000),
+            new Employee("Nikolay Petrov Сергеевич", 1, 110000),
+            new Employee("Dmitriy Danilov Вячеславович", 2, 250000),
+            new Employee("Maxim Kotov Николаевич", 3, 190000),
+            new Employee("Viktor Malorodov Иванович", 4, 170000),
+            new Employee("Timur Merekeshev Алексеевич", 5, 130000),
+    };
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    public static void printEmployees() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+
+    public static int calculateTotalSalary() {
+        int sum = 0;
+        for (Employee employee : employees) {
+            sum += employee.getSalary();
+        }
+        return sum;
+    }
+
+    public static Employee findEmploeeWithMinSalary(){
+        Employee employeeWithMinSalary = employees[0];
+
+        for (Employee employee : employees) {
+            if (employee.getSalary() < employeeWithMinSalary.getSalary()) {
+                employeeWithMinSalary = employee;
+            }
+        }
+        return employeeWithMinSalary;
+    }
+
+    public static Employee findEmploeeWithMaxSalary(){
+        Employee employeeWithMaxSalary = employees[0];
+
+        for (Employee employee : employees) {
+            if (employee.getSalary() > employeeWithMaxSalary.getSalary()) {
+                employeeWithMaxSalary = employee;
+            }
+        }
+        return employeeWithMaxSalary;
+    }
+
+    public static float calculateAverageSalary() {
+        return calculateTotalSalary() / employees.length;
     }
 }
